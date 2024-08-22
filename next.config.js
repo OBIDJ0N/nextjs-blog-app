5a51b678282bb1ce5ed464bc916a0d1c43a1ccd1
OBIDJ0N <olimovmuhammaddiyor5@gmail.com>
2024-08-22 15:30:05 +0500

    nextconfig

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
 	images: {
-		domains: ['media.graphassets.com'],
+		domains: ['us-east-1-shared-usea1-02.graphassets.com', 'media.graphassets.com'], 
 	},
 };
 
